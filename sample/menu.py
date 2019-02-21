a5e4725ddb1806a35c535010a43b7ad316ea6501
vkopio <ville.kopio@gmail.com>
2019-02-22 00:23:14 +0200

    Formatted week list

--- sample/menu.py
+++ sample/menu.py
@@ -11,13 +11,25 @@ def __get_menu():
     return todays_list
 
 
-def get_todays_menu():
-    todays_list = __get_menu().find("div", {"class": "row-today"})
-    date = todays_list.find("div", {"class": "col-day"}).p.get_text().capitalize()
-    food = todays_list.find("div", {"class": "col-food"}).p.get_text()
+def __format_list(days_list):
+    date = days_list.find("div", {"class": "col-day"}).p.get_text().capitalize()
+    food = days_list.find("div", {"class": "col-food"}).p.get_text()
 
     return '*{}*\n{}'.format(date, food)
 
 
+def get_todays_menu():
+    todays_list = __get_menu().find("div", {"class": "row-today"})
+
+    return __format_list(todays_list)
+
+
 def get_weeks_menu():
-    return __get_menu().get_text()
+    weeks_list = __get_menu().find_all("div", {"class": "row"})
+    formatted_list = ''
+
+    for days_list in weeks_list:
+        formatted_list += __format_list(days_list)
+        formatted_list += '\n'
+
+    return formatted_list
