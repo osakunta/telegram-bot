aea3ad0d38a45c9df7b7fc3dbe487061f8af937c
vkopio <ville.kopio@gmail.com>
2019-02-21 23:22:55 +0200

    Added /today command to the bot

--- sample/bot.py
+++ sample/bot.py
@@ -2,7 +2,7 @@ from menu import get_todays_menu
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.send_message(chat_id=update.message.chat_id, text="Use command /today to get today's menu!")
 
 
 def today(bot, update):
--- sample/main.py
+++ sample/main.py
@@ -2,7 +2,7 @@ import os
 import logging
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler
-from bot import start
+from bot import start, today
 
 load_dotenv()
 
@@ -13,7 +13,11 @@ logging.basicConfig(
 
 updater = Updater(token=os.getenv('TOKEN'))
 dispatcher = updater.dispatcher
+
 start_handler = CommandHandler('start', start)
+today_handler = CommandHandler('today', today)
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(today_handler)
+
 updater.start_polling()
