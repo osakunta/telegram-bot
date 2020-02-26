70858275a7581cfd5758d074ade8e92099a8d757
Ville Kopio <ville.kopio@kodan.fi>
2020-02-26 09:40:44 +0200

    Add checks to instruction parsing to prevent crashing.

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import telegram
 from telegram_bot.bot import execute_bot_command
 from telegram_bot.mocks import bot as bot_mock, update as update_mock
 
+def parse_instructions(update):
+    if update and update.message and isinstance(update.message.text, str):
+        instructions = update.message.text.split()
+        command = instructions[0].split('@', 1)[0]
+        args = instructions[1:]
+
+        return command, args
+
+    return None, None
+
 def telegram_bot(request):
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,11 +23,9 @@ def telegram_bot(request):
 
     bot = telegram.Bot(token=os.getenv('TOKEN'))
 
-    if request.method == "POST":
+    if request and request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
-        instructions = update.message.text.split()
-        command = instructions[0].split('@', 1)[0]
-        args = instructions[1:]
+        command, args = parse_instructions(update)
 
         execute_bot_command(command, args, bot, update)
 
